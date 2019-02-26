78de6e0d7815c4b83e5806b2b74d7aa60af5b8e1
potsables <postables@rtradetechnologies.com>
2019-02-25 19:13:04 -0800

    api/v2: add ipfs test refactors

--- api/v2/routes_rtfs_test.go
+++ api/v2/routes_rtfs_test.go
@@ -2,10 +2,8 @@ package v2
 
 import (
 	"bytes"
-	"encoding/json"
 	"errors"
 	"io"
-	"io/ioutil"
 	"mime/multipart"
 	"net/http/httptest"
 	"net/url"
@@ -70,6 +68,11 @@ func Test_API_Routes_IPFS_PIN(t *testing.T) {
 			}
 			// load fake rtfs
 			api.ipfs = fakeIPFS
+			// authentication
+			header, err := loginHelper(api, testUser, testUserPass)
+			if err != nil {
+				t.Fatal(err)
+			}
 			// a successful response needs to setup 2 mock stat calls
 			fakeIPFS.StatReturnsOnCall(0, &shell.ObjectStats{
 				CumulativeSize: tt.args.size,
@@ -80,8 +83,332 @@ func Test_API_Routes_IPFS_PIN(t *testing.T) {
 			urlValues := url.Values{}
 			urlValues.Add("hold_time", tt.args.holdTime)
 			var apiResp apiResponse
-			if err := sendRequest(
-				api, "POST", "/v2/ipfs/public/pin/"+tt.args.hash, tt.wantStatus, nil, urlValues, &apiResp,
+			if err := sendRequestWithAuth(
+				api, "POST", "/v2/ipfs/public/pin/"+tt.args.hash, header, tt.wantStatus, nil, urlValues, &apiResp,
+			); err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func Test_API_Routes_IPFS_File_Add(t *testing.T) {
+
+	type args struct {
+		filePath string
+		holdTime string
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus int
+	}{
+		{"Success", args{"../../testenv/config.json", "1"}, 200},
+		{"Failure-Bad-Hold-Time", args{"../../testenv/config.json", "notatime"}, 400},
+		{"Failure-Hold-Time-To-Long", args{"../../testenv/config.json", "1000000"}, 400},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// load configuration
+			cfg, err := config.LoadConfig("../../testenv/config.json")
+			if err != nil {
+				t.Fatal(err)
+			}
+			db, err := loadDatabase(cfg)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// setup fake mock clients
+			fakeLens := &mocks.FakeLensV2Client{}
+			fakeOrch := &mocks.FakeServiceClient{}
+			fakeSigner := &mocks.FakeSignerClient{}
+			fakeManager := &mocks.FakeManager{}
+
+			api, _, err := setupAPI(fakeLens, fakeOrch, fakeSigner, cfg, db)
+			if err != nil {
+				t.Fatal(err)
+			}
+			api.ipfs = fakeManager
+			// authentication
+			header, err := loginHelper(api, testUser, testUserPass)
+			if err != nil {
+				t.Fatal(err)
+			}
+			bodyBuf := &bytes.Buffer{}
+			bodyWriter := multipart.NewWriter(bodyBuf)
+			fileWriter, err := bodyWriter.CreateFormFile("file", tt.args.filePath)
+			if err != nil {
+				t.Fatal(err)
+			}
+			fh, err := os.Open(tt.args.filePath)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer fh.Close()
+			if _, err = io.Copy(fileWriter, fh); err != nil {
+				t.Fatal(err)
+			}
+			bodyWriter.Close()
+			testRecorder := httptest.NewRecorder()
+			req := httptest.NewRequest("POST", "/v2/ipfs/public/file/add", bodyBuf)
+			req.Header.Add("Authorization", header)
+			req.Header.Add("Content-Type", bodyWriter.FormDataContentType())
+			urlValues := url.Values{}
+			urlValues.Add("hold_time", tt.args.holdTime)
+			req.PostForm = urlValues
+			api.r.ServeHTTP(testRecorder, req)
+			if testRecorder.Code != tt.wantStatus {
+				t.Fatal("bad http status code recovered from /v2/ipfs/public/file/add")
+			}
+		})
+	}
+}
+
+func Test_API_Routes_IPFS_Add_Directory(t *testing.T) {
+	devValue := dev
+	type args struct {
+		filePath string
+		holdTime string
+	}
+	tests := []struct {
+		name        string
+		args        args
+		wantStatus  int
+		addDirError error
+		isDev       bool
+	}{
+		{"Success", args{"../../testfiles/testenv.zip", "1"}, 200, nil, true},
+		{"Failure-Not-Dev", args{"../../testfiles/testenv.zip", "1"}, 400, nil, false},
+		{"Failure-Not-Zip", args{"../../testenv/config.json", "1"}, 400, nil, true},
+		{"Failure-Bad-Hold-Time", args{"../../testenv/config.json", "notatime"}, 400, nil, true},
+		{"Failure-Hold-Time-To-Long", args{"../../testenv/config.json", "1000000"}, 400, nil, true},
+		// insert badd add
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// load configuration
+			cfg, err := config.LoadConfig("../../testenv/config.json")
+			if err != nil {
+				t.Fatal(err)
+			}
+			db, err := loadDatabase(cfg)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// setup fake mock clients
+			fakeLens := &mocks.FakeLensV2Client{}
+			fakeOrch := &mocks.FakeServiceClient{}
+			fakeSigner := &mocks.FakeSignerClient{}
+			fakeManager := &mocks.FakeManager{}
+
+			api, _, err := setupAPI(fakeLens, fakeOrch, fakeSigner, cfg, db)
+			if err != nil {
+				t.Fatal(err)
+			}
+			api.ipfs = fakeManager
+			fakeManager.AddDirReturnsOnCall(0, "hashyboi", tt.addDirError)
+			// authentication
+			header, err := loginHelper(api, testUser, testUserPass)
+			if err != nil {
+				t.Fatal(err)
+			}
+			bodyBuf := &bytes.Buffer{}
+			bodyWriter := multipart.NewWriter(bodyBuf)
+			fileWriter, err := bodyWriter.CreateFormFile("file", tt.args.filePath)
+			if err != nil {
+				t.Fatal(err)
+			}
+			fh, err := os.Open(tt.args.filePath)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer fh.Close()
+			if _, err = io.Copy(fileWriter, fh); err != nil {
+				t.Fatal(err)
+			}
+			bodyWriter.Close()
+			testRecorder := httptest.NewRecorder()
+			req := httptest.NewRequest("POST", "/v2/ipfs/public/file/add/directory", bodyBuf)
+			req.Header.Add("Authorization", header)
+			req.Header.Add("Content-Type", bodyWriter.FormDataContentType())
+			urlValues := url.Values{}
+			urlValues.Add("hold_time", tt.args.holdTime)
+			req.PostForm = urlValues
+			dev = tt.isDev
+			api.r.ServeHTTP(testRecorder, req)
+			if testRecorder.Code != tt.wantStatus {
+				t.Fatal("bad http status code recovered from /v2/ipfs/public/file/add")
+			}
+		})
+	}
+	// reset dev value
+	dev = devValue
+}
+
+func Test_API_Routes_IPFS_Pubsub_Publish(t *testing.T) {
+	type args struct {
+		topic, message string
+	}
+	tests := []struct {
+		name                string
+		args                args
+		wantStatus          int
+		pubSubError         error
+		increasePubsubcount int64
+	}{
+		{"Success", args{"foo", "bar"}, 200, nil, 0},
+		{"Failure-Missing-Message", args{"foo", ""}, 400, nil, 0},
+		{"Failure-Bad-Publish", args{"foo", "bar"}, 400, errors.New("bad"), 0},
+		{"Failure-Too-Many-Pubsubs", args{"foo", "bar"}, 400, nil, 1000},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// load configuration
+			cfg, err := config.LoadConfig("../../testenv/config.json")
+			if err != nil {
+				t.Fatal(err)
+			}
+			db, err := loadDatabase(cfg)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// setup fake mock clients
+			fakeLens := &mocks.FakeLensV2Client{}
+			fakeOrch := &mocks.FakeServiceClient{}
+			fakeSigner := &mocks.FakeSignerClient{}
+			fakeIPFS := &mocks.FakeManager{}
+			// setup fake api
+			api, _, err := setupAPI(fakeLens, fakeOrch, fakeSigner, cfg, db)
+			if err != nil {
+				t.Fatal(err)
+			}
+			// load fake rtfs
+			api.ipfs = fakeIPFS
+			// authentication
+			header, err := loginHelper(api, testUser, testUserPass)
+			if err != nil {
+				t.Fatal(err)
+			}
+			urlValues := url.Values{}
+			urlValues.Add("message", tt.args.message)
+			var intAPIResp interfaceAPIResponse
+			fakeIPFS.PubSubPublishReturnsOnCall(0, tt.pubSubError)
+			if tt.increasePubsubcount != 0 {
+				if err := api.usage.IncrementPubSubUsage(testUser, tt.increasePubsubcount); err != nil {
+					t.Fatal(err)
+				}
+			}
+			if err := sendRequestWithAuth(
+				api, "POST", "/v2/ipfs/public/pubsub/publish/"+tt.args.topic, header, tt.wantStatus, nil, urlValues, &intAPIResp,
+			); err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func Test_API_Routes_IPFS_Object_Stat(t *testing.T) {
+	type args struct {
+		hash string
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus int
+		statError  error
+	}{
+		{"Success", args{goodTestPinHash}, 200, nil},
+		{"Failure-Bad-Hash", args{badTestPinHash}, 400, nil},
+		{"Failure-Object-Stat-Error", args{goodTestPinHash}, 400, errors.New("bad")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// load configuration
+			cfg, err := config.LoadConfig("../../testenv/config.json")
+			if err != nil {
+				t.Fatal(err)
+			}
+			db, err := loadDatabase(cfg)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// setup fake mock clients
+			fakeLens := &mocks.FakeLensV2Client{}
+			fakeOrch := &mocks.FakeServiceClient{}
+			fakeSigner := &mocks.FakeSignerClient{}
+			fakeIPFS := &mocks.FakeManager{}
+
+			api, _, err := setupAPI(fakeLens, fakeOrch, fakeSigner, cfg, db)
+			if err != nil {
+				t.Fatal(err)
+			}
+			api.ipfs = fakeIPFS
+			// authentication
+			header, err := loginHelper(api, testUser, testUserPass)
+			if err != nil {
+				t.Fatal(err)
+			}
+			fakeIPFS.StatReturnsOnCall(0, &shell.ObjectStats{CumulativeSize: 100}, tt.statError)
+			var intAPIResp interfaceAPIResponse
+			if err := sendRequestWithAuth(
+				api, "GET", "/v2/ipfs/public/stat/"+tt.args.hash, header, tt.wantStatus, nil, nil, &intAPIResp,
+			); err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func Test_API_Routes_IPFS_Dag_Get(t *testing.T) {
+	type args struct {
+		hash string
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus int
+		dagError   error
+	}{
+		{"Success", args{goodTestPinHash}, 200, nil},
+		{"Failure-Bad-Hash", args{badTestPinHash}, 400, nil},
+		{"Failure-Dag-Get-Error", args{goodTestPinHash}, 400, errors.New("bad")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// load configuration
+			cfg, err := config.LoadConfig("../../testenv/config.json")
+			if err != nil {
+				t.Fatal(err)
+			}
+			db, err := loadDatabase(cfg)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// setup fake mock clients
+			fakeLens := &mocks.FakeLensV2Client{}
+			fakeOrch := &mocks.FakeServiceClient{}
+			fakeSigner := &mocks.FakeSignerClient{}
+			fakeIPFS := &mocks.FakeManager{}
+
+			api, _, err := setupAPI(fakeLens, fakeOrch, fakeSigner, cfg, db)
+			if err != nil {
+				t.Fatal(err)
+			}
+			api.ipfs = fakeIPFS
+			// authentication
+			header, err := loginHelper(api, testUser, testUserPass)
+			if err != nil {
+				t.Fatal(err)
+			}
+			fakeIPFS.DagGetReturnsOnCall(0, tt.dagError)
+			var intAPIResp interfaceAPIResponse
+			if err := sendRequestWithAuth(
+				api, "GET", "/v2/ipfs/public/dag/"+tt.args.hash, header, tt.wantStatus, nil, nil, &intAPIResp,
 			); err != nil {
 				t.Fatal(err)
 			}
@@ -105,7 +432,7 @@ func Test_API_Routes_IPFS_Public(t *testing.T) {
 	fakeOrch := &mocks.FakeServiceClient{}
 	fakeSigner := &mocks.FakeSignerClient{}
 
-	api, testRecorder, err := setupAPI(fakeLens, fakeOrch, fakeSigner, cfg, db)
+	api, _, err := setupAPI(fakeLens, fakeOrch, fakeSigner, cfg, db)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -113,49 +440,6 @@ func Test_API_Routes_IPFS_Public(t *testing.T) {
 	if err := api.usage.UpdateTier("testuser", models.Plus); err != nil {
 		t.Fatal(err)
 	}
-
-	// add a file normally
-	// /v2/ipfs/public/file/add
-	bodyBuf := &bytes.Buffer{}
-	bodyWriter := multipart.NewWriter(bodyBuf)
-	fileWriter, err := bodyWriter.CreateFormFile("file", "../../testenv/config.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-	fh, err := os.Open("../../testenv/config.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer fh.Close()
-	if _, err = io.Copy(fileWriter, fh); err != nil {
-		t.Fatal(err)
-	}
-	bodyWriter.Close()
-	testRecorder = httptest.NewRecorder()
-	req := httptest.NewRequest("POST", "/v2/ipfs/public/file/add", bodyBuf)
-	req.Header.Add("Authorization", authHeader)
-	req.Header.Add("Content-Type", bodyWriter.FormDataContentType())
-	urlValues := url.Values{}
-	urlValues.Add("hold_time", "5")
-	req.PostForm = urlValues
-	api.r.ServeHTTP(testRecorder, req)
-	if testRecorder.Code != 200 {
-		t.Fatal("bad http status code recovered from /v2/ipfs/public/file/add")
-	}
-	var apiResp apiResponse
-	// unmarshal the response
-	bodyBytes, err := ioutil.ReadAll(testRecorder.Result().Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err = json.Unmarshal(bodyBytes, &apiResp); err != nil {
-		t.Fatal(err)
-	}
-	// validate the response code
-	if apiResp.Code != 200 {
-		t.Fatal("bad api status code from /v2/ipfs/public/file/add")
-	}
-	hash = apiResp.Response
 
 	// temporary fix for a badly written this
 	// this will be solved in test refactoring
@@ -166,114 +450,6 @@ func Test_API_Routes_IPFS_Public(t *testing.T) {
 			Encrypted:   false,
 		},
 	)
-	// add a zip file (only do a partial test since this is being weird)
-	// /v2/ipfs/public/file/add/directory
-	bodyBuf = &bytes.Buffer{}
-	bodyWriter = multipart.NewWriter(bodyBuf)
-	fileWriter, err = bodyWriter.CreateFormFile("file", "../../testfiles/testenv.zip")
-	if err != nil {
-		t.Fatal(err)
-	}
-	fh, err = os.Open("../../testfiles/testenv.zip")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer fh.Close()
-	if _, err = io.Copy(fileWriter, fh); err != nil {
-		t.Fatal(err)
-	}
-	bodyWriter.Close()
-	testRecorder = httptest.NewRecorder()
-	req = httptest.NewRequest("POST", "/v2/ipfs/public/file/add/directory", bodyBuf)
-	req.Header.Add("Authorization", authHeader)
-	req.Header.Add("Content-Type", bodyWriter.FormDataContentType())
-	urlValues = url.Values{}
-	urlValues.Add("hold_time", "5")
-	req.PostForm = urlValues
-	api.r.ServeHTTP(testRecorder, req)
-
-	// test pubsub publish (success)
-	// /v2/ipfs/pubsub/publish/topic
-	urlValues = url.Values{}
-	urlValues.Add("message", "bar")
-	var mapAPIResp mapAPIResponse
-	if err := sendRequest(
-		api, "POST", "/v2/ipfs/public/pubsub/publish/foo", 200, nil, urlValues, &mapAPIResp,
-	); err != nil {
-		t.Fatal(err)
-	}
-	// validate the response code
-	if mapAPIResp.Code != 200 {
-		t.Fatal("bad api status code from  /v2/pubsub/publish/topic")
-	}
-	if mapAPIResp.Response["topic"] != "foo" {
-		t.Fatal("bad response")
-	}
-	if mapAPIResp.Response["message"] != "bar" {
-		t.Fatal("bad response")
-	}
-
-	// test pubsub publish (fail)
-	// /v2/ipfs/pubsub/publish/topic
-	urlValues = url.Values{}
-	urlValues.Add("message", "")
-	apiResp = apiResponse{}
-	if err := sendRequest(
-		api, "POST", "/v2/ipfs/public/pubsub/publish/foo", 400, nil, urlValues, &apiResp,
-	); err != nil {
-		t.Fatal(err)
-	}
-	if apiResp.Code != 400 {
-		t.Fatal("bad response status code from /v2/ipfs/public/pubsub/publish")
-	}
-
-	// test object stat (success)
-	// /v2/ipfs/stat
-	var interfaceAPIResp interfaceAPIResponse
-	if err := sendRequest(
-		api, "GET", "/v2/ipfs/public/stat/"+hash, 200, nil, nil, &interfaceAPIResp,
-	); err != nil {
-		t.Fatal(err)
-	}
-	if interfaceAPIResp.Code != 200 {
-		t.Fatal("bad response status code from /v2/ipfs/public/stat")
-	}
-
-	// test object stat (fail)
-	// /v2/ipfs/stat
-	interfaceAPIResp = interfaceAPIResponse{}
-	if err := sendRequest(
-		api, "GET", "/v2/ipfs/public/stat/notarealhash", 400, nil, nil, &interfaceAPIResp,
-	); err != nil {
-		t.Fatal(err)
-	}
-	if interfaceAPIResp.Code != 400 {
-		t.Fatal("bad response status code from /v2/ipfs/public/stat")
-	}
-
-	// test get dag (success)
-	// /v2/ipfs/public/dag
-	interfaceAPIResp = interfaceAPIResponse{}
-	if err := sendRequest(
-		api, "GET", "/v2/ipfs/public/dag/"+hash, 200, nil, nil, &interfaceAPIResp,
-	); err != nil {
-		t.Fatal(err)
-	}
-	if interfaceAPIResp.Code != 200 {
-		t.Fatal("bad response status code from /v2/ipfs/public/dag/")
-	}
-
-	// test get dag (fail)
-	// /v2/ipfs/public/dag
-	interfaceAPIResp = interfaceAPIResponse{}
-	if err := sendRequest(
-		api, "GET", "/v2/ipfs/public/dag/notarealhash", 400, nil, nil, &interfaceAPIResp,
-	); err != nil {
-		t.Fatal(err)
-	}
-	if interfaceAPIResp.Code != 400 {
-		t.Fatal("bad response status code from /v2/ipfs/public/dag/")
-	}
 
 	// test download
 	// /v2/ipfs/utils/download
@@ -285,7 +461,7 @@ func Test_API_Routes_IPFS_Public(t *testing.T) {
 
 	// test public network beam
 	// /v2/ipfs/utils/laser/beam
-	urlValues = url.Values{}
+	urlValues := url.Values{}
 	urlValues.Add("source_network", "public")
 	urlValues.Add("destination_network", "public")
 	urlValues.Add("content_hash", hash)
@@ -299,7 +475,7 @@ func Test_API_Routes_IPFS_Public(t *testing.T) {
 	// /v2/ipfs/public/pin/:hash/extend
 	urlValues = url.Values{}
 	urlValues.Add("hold_time", "5")
-	apiResp = apiResponse{}
+	apiResp := apiResponse{}
 	if err := sendRequest(
 		api, "POST", "/v2/ipfs/public/pin/"+hash+"/extend", 200, nil, urlValues, &apiResp,
 	); err != nil {
